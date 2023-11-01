dd55685214b0f70bd98ffd69c6ee4483c1c897e2
vlajich <d.vlajic88@gmail.com>
2023-11-01 14:03:45 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6c04b0f8-3dc0-4a3b-9807-6355892b90ad';
+export const MODAL_CLOSE_SEC = 2.5;
